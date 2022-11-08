0c2ce239d833d2043a9c2acaf23c08a8680ad6af
Noah <happyjaeho0221@gmail.com>
2022-11-08 10:21:49 -0600

    update

---
+++ 006_ATableOfVariables/ATableOfVariables.java
@@ -0,0 +1,25 @@
+public class HelloWorld
+{
+    public static void main(String[] arge)
+    {
+
+        System.out.println("a    a^2    a^3");
+        int a = 1;
+        int a2 = a * a;
+        int a3 = a * a * a;
+        System.out.println("1    " + a2 "    " + a3);
+        int a = 2;
+        int a2 = a * a;
+        int a3 = a * a * a;
+        System.out.println("2    " + a2 "    " + a3);
+        int a = 3;
+        int a2 = a * a;
+        int a3 = a * a * a;
+        System.out.println("3    " + a2 "    " + a3);
+        int a = 4;
+        int a2 = a * a;
+        int a3 = a * a * a;
+        System.out.println("4    " + a2 "    " + a3);
+     
+    }
+}
