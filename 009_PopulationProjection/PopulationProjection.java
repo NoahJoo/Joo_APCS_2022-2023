870a1c4b31177644c4aa9dfa2f00aa292f3dc24a
Noah <happyjaeho0221@gmail.com>
2022-11-21 10:22:47 -0600

    YOU LEFT IT OPEN!

---
+++ 009_PopulationProjection/PopulationProjection.java
@@ -0,0 +1,22 @@
+public class PopulationProjection
+{
+    {
+        public static void main(String[] arge)
+        
+        double birth = 7;
+        double death = 13;
+        double immigrant = 45;
+        double aYear = 60*60*24*365;
+
+        double birthAYear = aYear / birth;
+        double deathAYear = aYear / death;
+        double immigrantAYear = aYear / immigrant;
+
+        double birthFYear = birthAYear * 5;
+        double deathFYear = deathAYear * 5;
+        double immigrantFYear = ImmigrantAYear * 5;
+
+        System.out.println( + birthFYear - deathFYear + immigrantFYear )
+
+    }
+}1
---
+++ 031_IsItLegalForYou/IsItLegalForYou.java
@@ -0,0 +1,15 @@
+public class IsItLegalForYou {
+    public static void main(String[] arge)
+    {
+        //minnesota age restriction to 
+        // drivers license : 18
+        // rent a car : 21
+        // to get a job : 14
+        // to go to a casino : 18
+        // buy an alcohol :21
+        // medicare : 65
+        // running for a president : 35
+        // to buy a cig: 21
+
+    }
+}
