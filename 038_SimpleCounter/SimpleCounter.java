79e606d877053e5513d32d9558d2e2412931b0de
Noah <happyjaeho0221@gmail.com>
2022-11-28 10:11:22 -0600

    SimpleCounter/error?

--- 038_SimpleCounter/SimpleCounter.java
+++ 038_SimpleCounter/SimpleCounter.java
@@ -2,6 +2,16 @@ import java.util.Scanner
 
 public class SimpleCounter {
     public static void main(String[] arge) {
-        
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println(x: "Type any number you want");
+        int num = scan.nextIn();
+        int i = 1;
+
+        while (i <= num) {
+            i++;
+            System.out.println(+i);
+        }
+
     }
 }
