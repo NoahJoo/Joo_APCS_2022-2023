3ca1b37f11084ef505788fc6dd96aa4aa38357c9
Noah <happyjaeho0221@gmail.com>
2022-11-29 09:10:37 -0600

    SimpleCounterButAForLoop

---
+++ 040_SimpleCounterButAForLoop/SimpleCounterButAForLoop.java
@@ -0,0 +1,19 @@
+public class SimpleCounterButAForLoop {
+    public static void main(String[] args) {
+    
+    for (int i = 0; i <= 300; i++) {
+        System.out.println(+i);
+    }
+    
+    for (int j = 300; j >= 0; j--){
+        System.out.println(+j);
+    }
+    for (int a = 4798; a  <= 5324; a++) {
+        System.out.println(+a);
+    }
+    for (int b = 5324; b >= 4798; b--) {
+        System.out.println(+b);
+    }
+    
+    }
+}
