017e1d448defc75b3a178998141179595fbbe79d
Noah <happyjaeho0221@gmail.com>
2023-01-02 11:30:00 -0600

    SpellingBeeSimulatorUnfinished

---
+++ 044_SpellingBeeSimulator/SpellingBeeSimulator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SpellingBeeSimulator {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Type what you want to spell");
+        String spelling = scan.nextInt();
+        char spellingBee[] = {(0,1),(1,2), };
+
+        for (int i = 0; i <= spellingBee.length; i++) {
+            
+        }
+
+
+        do {
+            
+            while {
+
+            }
+        }
+
+
+    }
+}
---
+++ 045_BasicNestedPractice/BasicNestedPractice.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class BasicNestedPractice {
+
+    public static void main(String[] args) {
+
+
+    }
+}
